82de7410d63e20aebed3bcd161180662e6165f45
LoganPhilbrick <loganphilbrick99@gmail.com>
2025-04-20 18:43:33 -0400

    moved vidoe to top, playsinline

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import video from "./assets/initbl.mp4";
 import "./App.css";
-import AsciiOverlay from "./components/AsciiOverlay";
+// import AsciiOverlay from "./components/AsciiOverlay";
 
 function App() {
   return (
     <div>
-      <AsciiOverlay />
+      <div className="relative flex justify-center w-full h-screen overflow-hidden bg-black font-mono text-xs leading-none text-white">
+        <video
+          className="absolute top-0 w-full h-full object-cover z-10"
+          autoPlay
+          muted
+          loop
+          playsInline
+          src={video} // Replace with actual video source
+        />
+      </div>
+      {/* <AsciiOverlay /> */}
     </div>
   );
 }
